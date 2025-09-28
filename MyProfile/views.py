1e59b7b6b8509a8f70831d9ce7a11b7917ef7404
Behruz <roziyevbehruz123@gmail.com>
2025-09-28 21:22:49 +0300

    all project installed

---
+++ MyProfile/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def resume(request):
+    return render(request, 'resume.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
